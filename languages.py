f04e43737f120f918e10a1a40e20638fdc3dee8e
Olivier rishi matabaro <olivierrishimata@gmail.com>
2025-09-07 05:38:06 +0200

    Create languages.py

---
+++ languages.py
@@ -0,0 +1,13 @@
+# Language skills of Olivier Matabaro
+
+languages = {
+    "French": "Strong",
+    "English": "Intermediate",
+    "Spanish": "Intermediate",
+    "Swahili": "Strong"
+}
+
+print("Olivier's Language Skills:\n")
+
+for language, level in languages.items():
+    print(f"{language:<8} --> {level}")
